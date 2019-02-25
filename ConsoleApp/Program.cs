8a1c071cc9ab9dcd571f368d2072d3acd638311f
Alexandra Shkaryova <Sasha-021248@yandex.ru>
2019-02-25 16:03:52 +0300

    First commit

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using PublicTransport;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Autobus a = new Autobus();
+            a.AmountOfFuel = 5;
+            a.Capacity = 40;
+            a.ID = 153;
+            Autobus b = new Autobus { ID = 235, AmountOfFuel = 25, Capacity = 30 };
+            Autobus c = new Autobus { ID = 235, AmountOfFuel = 3, Capacity = 2 };
+            Tram t = new Tram { ID = 7, NumberOfWagon = 2 };
+            DispatchingOffice<Autobus> office = new DispatchingOffice<Autobus>;
+            office.AddVehicle(a);
+            office.AddVehicle(b);
+            office.AddVehicle(c);
+            office.AddVehicle(t);
+            
+            //office.SendMessageToVehicles();
+            a.SendInfoToDispatcher();
+            b.SendInfoToDispatcher();
+            c.SendInfoToDispatcher();
+            t.SendInfoToDispatcher();
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ PublicTransport/Class1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace PublicTransport
+{
+    public delegate void RecievedInformationHandler<TValue>(TValue T);
+
+    public abstract class Vehicle
+    {
+        public event RecievedInformationHandler<Vehicle> RecievedInformationEvent;
+        public int ID;
+
+        public void SendInfoToDispatcher()
+        {
+            if(RecievedInformationEvent!=null)
+                RecievedInformationEvent.Invoke(this);
+        }
+
+        public void RecieveMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    public class Autobus : Vehicle
+    {
+        public int Capacity;
+        public int AmountOfFuel;
+
+        public override string ToString()
+        {
+            return "ID:" + ID + ", Capacity:" + Capacity + ", amount of fuel:" + AmountOfFuel;
+        }
+    }
+
+    public class Tram : Vehicle
+    {
+        public int NumberOfWagon;
+
+        public override string ToString()
+        {
+            return "ID:" + ID + ", Number of wagon: " + NumberOfWagon;
+        }
+    }
+
+    public class DispatchingOffice<TValue> where TValue: Vehicle
+    {
+        public List<TValue> Vehicles;
+        public string VehicleType;
+        public HashSet<int> BusyNumbers;
+        public int NextNumber;
+        public DispatchingOffice()
+        {
+            Vehicles = new List<TValue>();
+            NextNumber = 1;
+            BusyNumbers = new HashSet<int>();
+        }
+
+        public void SendMessageToVehicles(TValue T)
+        {
+            T.RecieveMessage("Safe journey, " + T.ID+"!");
+        }
+
+        public void OnRecievedInfo(TValue T)
+        {
+            Console.WriteLine(T.ToString());
+            SendMessageToVehicles(T);
+        }
+
+        public void AddVehicle(Vehicle vehicle)
+        {
+            vehicle.ID = NextNumber;
+            NextNumber++;
+            Vehicles.Add(vehicle);
+            BusyNumbers.Add(vehicle.ID);
+            vehicle.RecievedInformationEvent += OnRecievedInfo;
+            
+        }
+    }
+}
